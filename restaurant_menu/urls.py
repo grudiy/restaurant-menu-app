dbdc569c29e0dac004b68a2db579f5d5fde2ec67
gruand <gruand@gmail.com>
2024-03-20 15:23:14 +0200

    Chagne in routing: menu-detail/

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,5 +3,5 @@ from restaurant_menu import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='menu'),
-    path('/menu-detail.html', views.MenuItemDetail.as_view(), name='menu-detail')
+    path('menu-detail/', views.MenuItemDetail.as_view(), name='menu-detail')
 ]
